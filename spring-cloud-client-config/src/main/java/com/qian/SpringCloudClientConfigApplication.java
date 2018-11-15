213db016472c3c8420f202ac24fa326d5438e253
qian <fengqian2018@dingtalk.com>
2018-11-15 10:50:35 +0800

    config

--- spring-cloud-client-config/src/main/java/com/qian/SpringCloudClientConfigApplication.java
+++ spring-cloud-client-config/src/main/java/com/qian/SpringCloudClientConfigApplication.java
@@ -3,6 +3,7 @@ package com.qian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author qian
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.qian"}, lazyInit = true)
 public class SpringCloudClientConfigApplication {
 
     public static void main(String[] args) {
