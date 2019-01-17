f0c4792b35d88738dc15a72bdfa12d30ce03128c
qian <fengqian2018@dingtalk.com>
2019-01-17 16:41:40 +0800

    commit test

---
+++ spring-cloud-test/src/main/java/com/qian/controller/WordImageController.java
@@ -0,0 +1,9 @@
+package com.qian.controller;
+
+/**
+ * @author qian
+ * @date 2019/1/16
+ */
+public class WordImageController {
+
+}
---
+++ spring-cloud-test1/src/test/java/moc/MocMvcTest.java
@@ -0,0 +1,9 @@
+package moc;
+
+/**
+ * @author qian
+ * @date 2018/11/20
+ */
+public class MocMvcTest {
+
+}
