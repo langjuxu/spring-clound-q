ed244b91ea614134bcbac93f373da4101921ad9b
qian <fengqian2018@dingtalk.com>
2019-01-29 10:49:43 +0800

    hhhhhhhhhhh

--- spring-cloud-test1/src/main/java/com/qian/controller/Test1Controller.java
+++ spring-cloud-test1/src/main/java/com/qian/controller/Test1Controller.java
@@ -30,8 +30,6 @@ public class Test1Controller {
     @PostMapping(value = "/mocTest1", produces = "application/json;charset=UTF-8")
     public String mocTest1(String key) {
         log.info("my key:{}", key);
-        log.info("lllllllllllllllllllllllll");
-        log.info("3333333333333333333333333");
         return key;
     }
 
