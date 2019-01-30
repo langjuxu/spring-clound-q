07b5c1ba312cfcf3f46cac48cf1e6de2ec3c9b9c
langjuxu <39114766+langjuxu@users.noreply.github.com>
2019-01-30 14:40:13 +0800

    Update TestController.java

--- spring-cloud-test/src/main/java/com/qian/controller/TestController.java
+++ spring-cloud-test/src/main/java/com/qian/controller/TestController.java
@@ -28,7 +28,7 @@ public class TestController {
 
     @GetMapping("/zipkin/{name}")
     public String testZipkin(@PathVariable String name) {
-        log.info("name:{}", "00000000000000000");
+        log.info("name:{}", "999999999999");
         System.err.println(name);
         ResponseEntity<String> exchange = restTemplate.exchange(test1ServiceUrl + name, HttpMethod.GET, null, String.class);
         System.err.println(exchange.getBody());
