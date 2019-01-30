47cedd631ed18cfd6eebfc093e96c4d67e358516
qian <fengqian2018@dingtalk.com>
2019-01-30 14:42:40 +0800

    dev conflict

--- spring-cloud-test/src/main/java/com/qian/controller/TestController.java
+++ spring-cloud-test/src/main/java/com/qian/controller/TestController.java
@@ -28,7 +28,7 @@ public class TestController {
 
     @GetMapping("/zipkin/{name}")
     public String testZipkin(@PathVariable String name) {
-        log.info("name:{}", "00000000000000000");
+        log.info("name:{}", "77777777777777777777");
         System.err.println(name);
         ResponseEntity<String> exchange = restTemplate.exchange(test1ServiceUrl + name, HttpMethod.GET, null, String.class);
         System.err.println(exchange.getBody());
